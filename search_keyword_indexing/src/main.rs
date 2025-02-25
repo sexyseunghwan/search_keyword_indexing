13748f920ee6b76f8a1a1a629bc571716222cbd8
ssh9308 <ssh9308@mediawill.com>
2025-02-25 17:16:33 +0900

    2025-02-25

---
+++ search_keyword_indexing/src/main.rs
@@ -0,0 +1,15 @@
+/*
+Author      : Seunghwan Shin
+Create date : 2025-03-00
+Description :
+
+History     : 2025-03-00 Seunghwan Shin       # [v.1.0.0] first create
+*/
+
+mod common;
+
+
+#[tokio::main]
+async fn main() {
+    println!("Hello, world!");
+}
